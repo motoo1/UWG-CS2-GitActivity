b62c47f9b36eaaa8eb672f49ec1f3155244ae86f
motoo1 <motoo1@my.westga.edu>
2025-09-23 14:39:58 -0400

    Updating some parts of the commits because it was messing up
    Implementing UI
    
    Made task description editable in UI and update the button associated method in the mainWindow(part 3A)

---
+++ code/Project1/src/test/java/edu/westga/cs1302/project1/test/model/task/TestConstructor.java
@@ -0,0 +1,14 @@
+package edu.westga.cs1302.project1.test.model.task;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestConstructor {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
