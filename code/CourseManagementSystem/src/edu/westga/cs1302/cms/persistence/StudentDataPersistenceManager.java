b53e76516266b98bdc8a7103b63b730b8227c980
motoo1 <motoo1@my.westga.edu>
2025-09-23 12:28:20 -0400

    Implement UI for adding tasks(Part 1 A)
    Added fields(textfield and textarea), combo box, button, labels and listview in FXML

--- code/CourseManagementSystem/src/edu/westga/cs1302/cms/persistence/StudentDataPersistenceManager.java
+++ code/CourseManagementSystem/src/edu/westga/cs1302/cms/persistence/StudentDataPersistenceManager.java
@@ -1,6 +1,14 @@
 package edu.westga.cs1302.cms.persistence;
 
+import java.io.File;
+import java.io.FileWriter;
+
 import edu.westga.cs1302.cms.model.Student;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 /** Suppports saving and loading student data,
  * 
@@ -16,8 +24,17 @@ public class StudentDataPersistenceManager {
 	 * 
 	 * @param students the set of students to save
 	 */
-	public static void saveStudentData(Student[] students) {
+	public static void saveStudentData(Student[] students) throws IOException {
+		try (FileWriter writer = new FileWriter("names.txt")) {
 		
+		for (Student currStudent : students) {
+			writer.write(currStudent.getName() + System.lineSeparator());
+			writer.write(currStudent.getGrade());
+			writer.write(System.lineSeparator());
+			
+		}
+
+	}
 	}
 
 	/** Load the students!
@@ -26,9 +43,28 @@ public class StudentDataPersistenceManager {
 	 * @postcondition none
 	 * 
 	 * @return the set of students loaded
+	 * @throws NumberFormatException file not properly formatted, failed while loading a grade value
+	 * @throws IOException 
+	 * @throws NoSuchElementException file is not formatted properly, failed to read line when trying to read a grade value
+	 * 
 	 */
-	public static Student[] loadStudentData() {
-		return null;
+	public static Student[] loadStudentData() throws NumberFormatException, IOException {
+		ArrayList<Student> students = new ArrayList<Student>();
+		File inputFile = new File("names.txt");
+		try (Scanner reader = new Scanner(inputFile)) {
+			while (reader.hasNextLine()) {
+				String name = reader.nextLine();
+				int grade = Integer.parseInt(reader.nextLine());
+				Student nextStudent = new Student(name, grade);
+				students.add(nextStudent);
+			}
+		} catch (NumberFormatException error) {
+			throw new IOException(error.getMessage());
+		} catch (NoSuchElementException error) {
+			throw new IOException(error.getMessage());
+		}
+		
+		return (Student []) students.toArray();
 	}
 	
 }
