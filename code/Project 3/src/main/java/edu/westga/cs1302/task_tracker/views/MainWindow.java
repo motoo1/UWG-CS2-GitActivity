a4c438e6616d6d7ef843de6b5baaa8caa7c55a4d
motoo1 <motoo1@my.westga.edu>
2025-12-04 19:05:26 -0500

    Input Validation for Storing Collections(PART 1E)
    Added input validation to disable Add collection button when name Textfield isempty

--- code/Project 3/src/main/java/edu/westga/cs1302/task_tracker/views/MainWindow.java
+++ code/Project 3/src/main/java/edu/westga/cs1302/task_tracker/views/MainWindow.java
@@ -1,4 +1,5 @@
 package edu.westga.cs1302.task_tracker.views;
+
 import edu.westga.cs1302.task_tracker.model.Collection;
 import edu.westga.cs1302.task_tracker.viewmodel.CollectionViewModel;
 import javafx.event.ActionEvent;
@@ -8,7 +9,7 @@ import javafx.scene.control.ContextMenu;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
-
+import javafx.scene.control.Button;
 
 /**
  * Controller class for MainWindow of the Task Tracker system.
@@ -20,27 +21,29 @@ public class MainWindow {
 	@FXML
 	private TextField collectionName;
 	@FXML
+	private Button addButton;
+	@FXML
 	private ListView<Collection> collections;
 	private final CollectionViewModel viewModel = new CollectionViewModel();
 		
 	@FXML
 	void addCollection(ActionEvent event) {
-	    if (viewModel.nameProperty().get().isBlank()) {
+	    if (this.viewModel.nameProperty().get().isBlank()) {
 	        Alert alert = new Alert(Alert.AlertType.WARNING, "Please enter a collection name ");
 	        alert.showAndWait();
 	        return;
 	    }
 
-	    viewModel.addCollection();	}
+	    this.viewModel.addCollection();	}
 
 	@FXML
 	void removeCollection(ActionEvent event) {
-	    if (viewModel.selectedCollectionProperty().get() == null) {
-	        Alert alert = new Alert(Alert.AlertType.WARNING,"Please select a collection to remove.");
+	    if (this.viewModel.selectedCollectionProperty().get() == null) {
+	        Alert alert = new Alert(Alert.AlertType.WARNING, "Please select a collection to remove.");
 	        alert.showAndWait();
 	        return;
 	    }
-	    viewModel.removeCollection();
+	    this.viewModel.removeCollection();
 
 	    	}
 
@@ -53,16 +56,17 @@ public class MainWindow {
 	 */
 	@FXML
 	public void initialize() {
+		this.addButton.disableProperty().bind(
+			    this.viewModel.nameProperty().isEmpty()
+			);
 		
-	collectionName.textProperty().bindBidirectional(viewModel.nameProperty());
-	collections.setItems(viewModel.getCollections());
-	viewModel.selectedCollectionProperty().bind(collections.getSelectionModel().selectedItemProperty());
+	this.collectionName.textProperty().bindBidirectional(this.viewModel.nameProperty());
+	this.collections.setItems(this.viewModel.getCollections());
+	this.viewModel.selectedCollectionProperty().bind(this.collections.getSelectionModel().selectedItemProperty());
 	ContextMenu collectionContextMenu = new ContextMenu();
 	MenuItem removeItem = new MenuItem("Remove Collection");
-	removeItem.setOnAction(e -> removeCollection(null));
+	removeItem.setOnAction(e -> this.removeCollection(null));
 	collectionContextMenu.getItems().add(removeItem);
 	this.collections.setContextMenu(collectionContextMenu);
 	}
-
-
 	}
